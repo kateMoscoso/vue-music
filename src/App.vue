f7874830cba0eb32f30b72b73993efe643da21bc
kateMoscoso <katiemscs@gmail.com>
2019-06-23 19:37:15 +0200

    connect api

--- src/App.vue
+++ src/App.vue
@@ -10,40 +10,38 @@
           )
           a.button.is-info.is-large(@click="search") Buscar
           a.button.is-danger.is-large &times;
-          p
-            small {{ searchMessage }}
+      .container
+        p
+          small {{ searchMessage }}
 
       .container.results
         .columns
-          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist}}
+          .column(v-for="t in tracks")
+            | {{ t.name }} - {{ t.artists[0].name }}
 </template>
 
 <script>
-const tracks = [
-  { name: 'Muchacha', artist: 'Luis Alberto Spinetta' },
-  { name: 'Hoy aca en el baile', artist: 'El Pepo' },
-  { name: 'I was made for loving you', artist: 'Kiss' }
-]
-
+import trackService from './services/track'
 export default {
   name: 'app',
-
   data () {
     return {
       searchQuery: '',
       tracks: []
     }
   },
-
   computed: {
     searchMessage () {
       return `Encontrados: ${this.tracks.length}`
     }
   },
-
   methods: {
     search () {
-      this.tracks = tracks
+      if (!this.searchQuery) { return }
+      trackService.search(this.searchQuery)
+        .then(res => {
+          this.tracks = res.tracks.items
+        })
     }
   }
 }
@@ -51,7 +49,6 @@ export default {
 
 <style lang="scss">
   @import './scss/main.scss';
-
   .results {
     margin-top: 50px;
   }

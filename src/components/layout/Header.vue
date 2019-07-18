a47232363126b54e4918aee587b56e99561e0af2
kateMoscoso <katiemscs@gmail.com>
2019-07-18 19:22:41 +0200

    changes

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  section.hero.is-mediu.is-primary.is-bold
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong 📻  Vue Music
+            .nav-right.nav-menu
+
+    .hero-body
+      .container.has-text-centered
+        h1.title Vue Music
+        h2.subtitle Canciones que estan Vue-nisimas!
+</template>

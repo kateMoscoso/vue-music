27af1c0f1aaa90d0061ace3e20eecce370b27868
kateMoscoso <katiemscs@gmail.com>
2019-08-11 23:08:16 +0200

    avances

--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  .card
+  .card(v-if="track && track.album")
     .card-image
       figure.image.is-1by1
         img(:src="track.album.images[0].url")
@@ -15,20 +15,34 @@
           p.subtitle.is-6 {{ track.artists[0].name }}
 
       .content
-        small {{ track.duration_ms }}
+        small {{ track.duration_ms | ms-to-mm }}
         nav.level
           .level-left
-            a.level-item
-              span.icon.is-small ▶️
+            button.level-item.button.is-primary
+              span.icon.is-small(@click="selectTrack") ▶️
+            button.level-item.button.is-warning
+              span.icon.is-small(@click="goToTrack(track.id)") 🌎
 </template>
 
 <script>
 export default {
   props: {
     track: { type: Object, required: true }
+  },
+
+  methods: {
+    selectTrack () {
+      if (!this.track.preview_url) { return }
+
+      this.$emit('select', this.track.id)
+      this.$bus.$emit('set-track', this.track)
+    },
+
+    goToTrack (id) {
+      if (!this.track.preview_url) { return }
+
+      this.$router.push({ name: 'track', params: { id } })
+    }
   }
 }
 </script>
-
-<style lang="css">
-</style>
--- webpack.config.js
+++ webpack.config.js
@@ -11,48 +11,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
-      },
-      {
-        test: /\.sass$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader?indentedSyntax'
-        ],
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [path.resolve(__dirname, './src')]
       },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader'
-            ],
-            'sass': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader?indentedSyntax'
-            ]
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-          // other vue-loader options go here
         }
       },
       {
@@ -71,14 +42,13 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
-    extensions: ['*', '.js', '.vue', '.json']
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, './src')
+    }
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true,
-    overlay: true
+    noInfo: false
   },
   performance: {
     hints: false
